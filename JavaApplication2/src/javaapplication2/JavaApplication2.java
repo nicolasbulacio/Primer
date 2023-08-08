84f5a2d4cbde34e124b3015bfa0891233f013063
IvaaNZ <husar2k16@gmail.com>
2023-08-08 16:14:42 -0300

    Tuki

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -16,6 +16,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         System.out.println("Hola git");
         System.out.println("Hola git, soy Ivan");
+        System.out.println("Tuki");
     }
     
 }
