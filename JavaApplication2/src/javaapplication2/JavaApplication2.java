dee95e34b2e7ddf09c0d01162fc77376e5b320fc
S3rgio17 <chexser17@gmail.com>
2023-08-08 16:58:56 -0300

    Update JavaApplication2.java
    cambio

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -1,32 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package javaapplication2;
 
-/**
- *
- * @author c15c0
- */
+import java.util.Scanner;
+
+
 public class JavaApplication2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
         System.out.println(" /////////////\\\\\\\\\\\\");
         System.out.println("Hola git");
         System.out.println("--------------------------");
         System.out.println("Hola git, soy Ivan");
-<<<<<<< HEAD
         System.out.println("--------------------------");
         System.out.println("Hola git, soy Sergio");
-        System.out.println("--------------------------");
-        
-        
-=======
+        System.out.println("--------------------------");             
         System.out.println("Tuki");
->>>>>>> 84f5a2d4cbde34e124b3015bfa0891233f013063
+        
+        
+        System.out.println("Hola, Cual es tu nombre???");
+        String nom = sc.next();
+        
+        System.out.println("Hola "+nom);
+        
     }
     
 }
