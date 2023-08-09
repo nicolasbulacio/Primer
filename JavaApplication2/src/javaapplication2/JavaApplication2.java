5b31dcd28a6fb4a26a653250bb2d2f0f84e31459
bulacio nicolas <93942151+nicolasbulacio@users.noreply.github.com>
2023-08-09 15:41:41 -0300

    asdfa
    sd

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -7,13 +7,16 @@ public class JavaApplication2 {
 
     
     public static void main(String[] args) {
+<<<<<<< HEAD
         
         Scanner sc = new Scanner(System.in);
         
         System.out.println(" /////////////\\\\\\\\\\\\");
+=======
+>>>>>>> parent of 8550872 (Merge branch 'main' of https://github.com/nicolasbulacio/Primer)
         System.out.println("Hola git");
-        System.out.println("--------------------------");
         System.out.println("Hola git, soy Ivan");
+<<<<<<< HEAD
         System.out.println("--------------------------");
         System.out.println("Hola git, soy Sergio");
         System.out.println("--------------------------");             
@@ -21,6 +24,8 @@ public class JavaApplication2 {
         System.out.println("--------------------------");
         
         System.out.println("Puto el que lee :O");
+=======
+>>>>>>> parent of 8550872 (Merge branch 'main' of https://github.com/nicolasbulacio/Primer)
         System.out.println("Tuki");
         System.out.println("Bonjour");
         
